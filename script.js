b38396dd35b05a452d1e6d8fbffa54fe761750d1
Email-12 <emil3h@gmail.com>
2023-04-15 03:06:21 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+  var image = document.getElementById("itsemil");
+  var opacity = 0;
+
+  var timer = setInterval(function() {
+    if (opacity >= 1) {
+      clearInterval(timer);
+    }
+    image.style.opacity = opacity;
+    image.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    opacity += 0.02;
+  }, 42);
+}); 
+*/
+
+window.onload = function() {
+  var image = document.getElementById("itsemil");
+  var opacity = 0;
+  image.style.display = 'block';
+
+  setTimeout(function() {
+    var timer = setInterval(function() {
+      if (opacity >= 1) {
+        clearInterval(timer);
+      }
+      image.style.opacity = opacity;
+      image.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+      opacity += 0.05;
+    }, 50);
+  }, 1000); // 1000 milliseconds = 1 second
+};
